00ff0ccbe3cfdf425e77dcacf4af1ddc7e35a1c7
Vikas <7014.vikas@gmail.com>
2024-09-29 19:37:39 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
 import "./App.css";
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Profile from "./pages/Profile"
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 import PostDetails from "./pages/PostDetails";
-import EditPost from "./pages/EditPost"
-import MyBlogs from "./pages/MyBlogs"
-import CreatePost from "./pages/CreatePost"
+import EditPost from "./pages/EditPost";
+import MyBlogs from "./pages/MyBlogs";
+import CreatePost from "./pages/CreatePost";
 
-import {Route , Routes} from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 import { UserContext } from "./context/UserContext";
 
 function App() {
   return (
     <UserContextProvider>
- <Routes>
-      <Route exact path='/' element={<Home/> }/>
-      <Route exact path='/login' element={<Login/> }/>
-      <Route exact path='/register' element={<Register/> }/>
-      <Route exact path='/write' element={<CreatePost/> }/>
-      <Route exact path='/Post/post/:id' element={<PostDetails/> }/>
-      <Route exact path='/edit/:id' element={<EditPost/> }/>
-      <Route exact path='/myblogs/:id' element={<MyBlogs/> }/>
-      <Route exact path='/profile/:id' element={<Profile/> }/>
-
-    </Routes> 
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/write" element={<CreatePost />} />
+        <Route exact path="/Post/post/:id" element={<PostDetails />} />
+        <Route exact path="/edit/:id" element={<EditPost />} />
+        <Route exact path="/myblogs/:id" element={<MyBlogs />} />
+        <Route exact path="/profile/:id" element={<Profile />} />
+      </Routes>
     </UserContextProvider>
-   
-    
   );
-
 }
 
 export default App;
